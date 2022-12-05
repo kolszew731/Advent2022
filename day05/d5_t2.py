1ae00bef1334b72a515324c474f221aafbc9e88e
Krzysztof Olszewski <kolszew73@gmail.com>
2022-12-05 18:05:33 +0100

    day5

---
+++ day05/d5_t2.py
@@ -0,0 +1,20 @@
+def solve():
+    stacks = [list("HTZD"), list("QRWTGCS"), list("PBFQNRCH"),
+              list("LCNFHZ"), list("GLFQS"), list("VPWZBRCS"),
+              list("ZFJ"), list("DLVZRHQ"), list("BHGNFZLD")]
+    with open('input.txt') as f:
+        lines = f.readlines()
+    lines = [l.replace("move ", "").replace(" from ", ",").replace(" to ", ",").strip() for l in lines]
+    for line in lines:
+        command = line.split(",")
+        count = int(command[0])
+        move_from = int(command[1]) - 1
+        move_to = int(command[2]) - 1
+        stacks[move_to].extend(stacks[move_from][-count:])
+        stacks[move_from] = stacks[move_from][:-count]
+    for stack in stacks:
+        print(stack.pop(), end="")
+
+
+if __name__ == '__main__':
+    solve()
